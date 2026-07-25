9af5a79cc2f161903a06e1e5eb32d29df0c92f5e
mist-days <timmyxzosaka@gmail.com>
2026-07-25 13:10:42 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/com/mistdays/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistdays.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
