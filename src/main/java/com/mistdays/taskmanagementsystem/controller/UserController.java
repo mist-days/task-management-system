f4b881e64ea4096c84dbcc592666ffaf370c3e74
mist-days <timmyxzosaka@gmail.com>
2026-07-30 23:15:47 +0900

    feat: implement create user API

--- src/main/java/com/mistdays/taskmanagementsystem/controller/UserController.java
+++ src/main/java/com/mistdays/taskmanagementsystem/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.mistdays.taskmanagementsystem.controller;
 
+import com.mistdays.taskmanagementsystem.entity.User;
+import com.mistdays.taskmanagementsystem.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
 }
